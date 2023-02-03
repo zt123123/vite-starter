8e4d3306d0566cdfe547a4f8e01f2fda8eebe88d
zhangtian <zhangtian@weidian.com>
2023-02-03 11:55:28 +0800

    :tada: chore: test

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,37 @@
-const env = import.meta.env.VITE_ENV
+function a() {
+  console.log('a');
 
-console.log(env)
+  b()
+}
+function b() {
+  for (let index = 0; index < 10000; index++) {
+    console.log('long task');
+  }
+  console.log('b');
+}
+setTimeout(function test3() {
+  console.log(9999);
+}, 1000);
+setTimeout(function test() {
+  setTimeout(function test2() {
+    a()
+  });
+}, 1000);
+
+
+
+function c() {
+  console.log('c');
+  return new Promise(function promise(resolve) {
+    setTimeout(() => {
+      console.log('promise');
+      resolve(null)
+    }, 9000);
+  })
+}
+setTimeout(async function test4() {
+  await c()
+  const h1 = document.createElement("h1")
+  h1.innerHTML = "world"
+  document.body.appendChild(h1)
+}, 2000);
